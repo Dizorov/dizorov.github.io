a91615b066e9270043f72d2b70c57b0f1e6d9b88
dizorov <dizorov666@gmail.com>
2020-03-23 22:45:00 +0300

    Добавил анимированный скролл из навигации

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 console.log('JS is work');
 
 
-var	navBar     = document.querySelector('.page-header__navbar'),
+const	navBar     = document.querySelector('.page-header__navbar'),
 	menuButton = document.querySelector('.navbar__menu-icon'),
 	menuIcon   = document.querySelector('.menu-icon__bar-line'),
 	navBarNav  = document.querySelector('.navbar__nav');
@@ -23,3 +23,26 @@ function menuToggler() {
 }
 
 menuButton.addEventListener('click', menuToggler);
+
+//Анимированный скролл к блоку из навигации
+
+navBar.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  let target, link;
+
+  if(e.target.closest('span')) {
+    link = e.target.parentElement.getAttribute('href');
+  } else {
+    if (e.target.closest('a')) {
+      link = e.target.getAttribute('href');
+    }
+  }
+
+  target = document.querySelector(`${link}`);
+  console.log(target.offsetTop);
+  window.scrollTo({
+    top: target.offsetTop,
+    behavior: 'smooth'
+  })
+})
